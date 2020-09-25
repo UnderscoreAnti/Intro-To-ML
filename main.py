79cabd89043dc95307404cf2f98ec8f99bbe6b84
UnderscoreAnti <UnderscoreAnti@gmail.com>
2020-09-25 14:24:59 -0600

    Commit before leaving for work

---
+++ main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as ny
+import matplotlib as mpl
+
+
+# Pandas reads the CSV files and coverts it into usable data
+df = pd.read_csv('titanicdata.csv')
+
+# This returns a table of statistics about each column
+pd.options.display.max_columns = 6
+print(df.describe())
+
+# This returns a single column, called a DATA SERIES
+column = df["Fare"]
+print(column)
+
+# And this returns a smaller data frame
+small_df = df["Age", "Survived", "Sex"]
+print(small_df.head())
+
+
+
+
