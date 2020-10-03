1963c5c2742dc1a7223455abe1b260ee3abf4639
UnderscoreAnti <UnderscoreAnti@gmail.com>
2020-10-02 21:16:02 -0600

    Removed print functions

--- main.py
+++ main.py
@@ -8,16 +8,16 @@ df = pd.read_csv('titanicdata.csv')
 
 # This returns a table of statistics about each column
 pd.options.display.max_columns = 6
-print(df.describe())
+# print(df.describe())
 
 # This returns a single column, called a DATA SERIES
 column = df["Fare"]
-print(column)
+# print(column)
 
 # And this returns a smaller data frame
 small_df = df[["Age", "Survived", "Sex"]]
-print(small_df.head())
+# print(small_df.head())
 
-
-
+# This converts the sex values from strings to booleans
+# df['male'] = df['Sex'] == 'male'
 
